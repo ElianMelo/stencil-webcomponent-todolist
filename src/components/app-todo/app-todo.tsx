babf3e856792328ad3b529c911fa238a83cc5076
ElianMelo <elianmelomorais@gmail.com>
2022-04-06 01:19:07 -0300

    Criação da lista todo em um único componente

--- src/components/app-todo/app-todo.tsx
+++ src/components/app-todo/app-todo.tsx
@@ -1,18 +1,44 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, h, State, Host } from '@stencil/core';
 
 @Component({
   tag: 'app-todo',
   styleUrl: 'app-todo.css',
   shadow: true,
 })
-export class AppTodo {
+export class TodoComponent {
+
+  @State() task: string = ''
+  @State() tasks: string[] = []
+
+  handleSubmit = (ev: Event) => {
+    ev.preventDefault();
+    if (this.task.trim()) {
+      this.tasks.push(this.task);
+      this.task = '';
+    }
+  }
+
+  handleInput = (ev: Event) => {
+    const field = ev.target as HTMLInputElement
+    this.task = field.value;
+
+  }
 
   render() {
     return (
-      <form>
-        <input></input>
-        <button>Adicionar</button>
-      </form>
+      <Host>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            onInput={this.handleInput}
+            value={this.task}
+          ></input>
+          <button>Adicionar</button>
+        </form>
+        <ul>
+          {this.tasks.map((t, i) => <li key={i}>{t}</li>)}
+        </ul>
+      </Host>
+
     );
   }
 
